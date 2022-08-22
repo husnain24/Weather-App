4d70e7f8af3e79389d9f8323010338b5a1f77fb8
Husnain <98628692+husnain24@users.noreply.github.com>
2022-08-21 21:38:54 -0600

    Added comments and restructured

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-// /**
-//  * Weather App
-//  * Author: Husnain Ahmad
-//  */
+/**
+ * Weather App
+ * Provides a multiple different types of weather data and displays
+ * it to the user.
+ * @Author Husnain Ahmad
+ */
 
+
+// Form elements
 const userInput = document.getElementById('user-input');
 const searchButton = document.getElementById('submit');
-
+// Different types of infomation elements to be populated
 const place = document.getElementById('location');
 const pic = document.getElementById('pic');
 const time = document.getElementById('time');
@@ -15,15 +19,19 @@ const temp = document.getElementById('temperature');
 
 searchButton.addEventListener('click', searchCity);
 
-
+/**
+ * Takes in the user inputted city and fetches the weather data
+ * JSON file. It then calls function to extract data from it.
+ * @param {*} event 
+ * @returns 
+ */
 async function searchCity (event) {
   event.preventDefault();
   let input = userInput.value;
   if(input == '')
     return;
   const weatherData = await getWeatherData(input);
-  console.log(weatherData);
-  // console.log()
+  // Validate city user inputs.
   if(weatherData.hasOwnProperty('error')) {
     alert('Input city correctly')
     return;
@@ -32,25 +40,32 @@ async function searchCity (event) {
   userInput.value = '';
 }
 
-
+/**
+ * Fills all the result elements with their specific data from the weather
+ * JSON file.
+ * @param {*} weatherData 
+ */
 function extractWeatherData(weatherData) {
-  // document.getElementById('weather-results').toggleAttribute('.results')
+  // Get the full location of the weather.
   place.innerText = weatherData.location.name + ', ' + weatherData.location.region + ", " + weatherData.location.country;
+  // Add the country's flag to its specific div.
   addCountryFlag(weatherData.location.country);
-  // place.innerText = weatherData.location.name + ", " + weatherData.location.country;
+  // Add the time of the specific location.
   time.innerText = weatherData.location.localtime;
+  // Add the description describing the weather.
   description.innerText = weatherData.current.condition.text;
-  temp.innerText = weatherData.current.temp_f + ' F';
-
+  // Add the image describing the weather.
   const imgLink = weatherData.current.condition.icon;
   const imgHtml = `<img id='image-weather' src="${imgLink}" height = 50px; width = 50px;>`
   picture.innerHTML = imgHtml;
+  // Add the temperature.
+  temp.innerText = weatherData.current.temp_f + ' F°';
 }
 
 /**
- * Retrieve weather data from openweathermap
+ * Retrieve weather data from Openweathermap api
  */
- const getWeatherData = (city) => {
+ function getWeatherData(city) {
   const options = {
     method: 'GET',
     headers: {
@@ -59,21 +74,18 @@ function extractWeatherData(weatherData) {
     }
   };
   return fetch(`https://weatherapi-com.p.rapidapi.com/current.json?q=${city}`, options)
-    .then(response => response.json()
-      // if(response.ok)
-      //   return response.json();
-      // else {
-      //   alert('Input city correctly')
-      // }
-    )
+    .then(response => response.json())
     .then(data => data)
     .catch(error => console.log(error));
 }
 
+/**
+ * Retriev and add the country's flag from restcountries.com api.
+ * @param {*} country 
+ */
 function addCountryFlag(country) {
   fetch(`https://restcountries.com/v3.1/name/${country}`)
 	.then(response => response.json())
 	.then(data => pic.innerHTML = `<img id='flag' src="${data[0].flags.png}">`)
 	.catch(err => console.error(err));
 }
-
